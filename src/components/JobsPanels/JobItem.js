30a14bd6c3f3fdcea41e39d684ff2c60a28324f3
nour-atef04 <nourmohamedatef04@gmail.com>
2026-01-17 23:16:44 +0200

    created save buttons for jobs

--- src/components/JobsPanels/JobItem.js
+++ src/components/JobsPanels/JobItem.js
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
 import styles from "./JobItem.module.css";
+import { FaRegBookmark } from "react-icons/fa";
+import { FaBookmark } from "react-icons/fa";
+import { useState } from "react";
 
-export default function JobItem({job, onClick }) {
+export default function JobItem({ job, onClick }) {
   const {
     _id,
     title,
@@ -12,17 +15,24 @@ export default function JobItem({job, onClick }) {
     descriptionBreakdown,
   } = job;
 
-  return (
-    <Link to={`/jobs/${_id}`} className={styles["job-item"]}>
-      <p className={styles["job-name"]} onClick={onClick}>
-        {title}
-      </p>
+  const [isSaved, setIsSaved] = useState(false);
 
-      <div className={styles["job-info"]}>
-        {owner.companyName} •{" "}
-        {owner.locationAddress || locationAddress || location} ({type}){" "}
-        {descriptionBreakdown && `• ${descriptionBreakdown.employmentType}`}
+  return (
+    <div className={styles["item-container"]}>
+      <Link to={`/jobs/${_id}`} className={styles["job-item"]}>
+        <p className={styles["job-name"]} onClick={onClick}>
+          {title}
+        </p>
+
+        <div className={styles["job-info"]}>
+          {owner.companyName} •{" "}
+          {owner.locationAddress || locationAddress || location} ({type}){" "}
+          {descriptionBreakdown && `• ${descriptionBreakdown.employmentType}`}
+        </div>
+      </Link>
+      <div className={styles.save} onClick={() => setIsSaved((prev) => !prev)}>
+        {!isSaved ? <FaRegBookmark /> : <FaBookmark />}
       </div>
-    </Link>
+    </div>
   );
 }
